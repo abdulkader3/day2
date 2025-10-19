e5870410f96d3730ab933e024f61464856f6e063
Nadia Mahbuba <nadiamahbuba.webdevsoln@gmail.com>
2025-10-19 10:52:55 +0600

    nadia_mahbuba_section_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Digital from './Nadia_Digital_clients/Digital'
+import Clients from './Nadia_Digital_clients/Clients'
 
 const App = () => {
   return (
     <div>
       <Digital/>
+      <Clients/>
     </div>
   )
 }
---
+++ src/Nadia_Digital_clients/Clients.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import happpy_client from '../assets/images/Happy Client.png'
+const Clients = () => {
+  return (
+    <section>
+<div className="container">
+    <img src={happpy_client} alt="" />
+</div>
+
+    </section>
+  )
+}
+
+export default Clients

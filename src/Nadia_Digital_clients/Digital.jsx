875391bceba0115e752148069e0f734d64610584
Nadia Mahbuba <nadiamahbuba.webdevsoln@gmail.com>
2025-10-19 09:00:23 +0600

    nadia

---
+++ src/Nadia_Digital_clients/Digital.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Digital = () => {
+  return (
+    <>
+    <section className='Digital'>
+      <div>
+
+      </div>
+      <div></div>
+    </section>
+    
+    </>
+  )
+}
+
+export default Digital

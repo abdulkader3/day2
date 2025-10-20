7d9dcee1e14575e10a18e0f01ffc73b2c0c789a5
Nadia Mahbuba <nadiamahbuba.webdevsoln@gmail.com>
2025-10-21 04:51:28 +0600

    section_3 AOS DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { Suspense, useEffect } from 'react'
 import Digital from './Nadia_Digital_clients/Digital'
 import Clients from './Nadia_Digital_clients/Clients'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const App = () => {
+    useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
     <div>
       <Digital/>
--- src/Nadia_Digital_clients/Clients.jsx
+++ src/Nadia_Digital_clients/Clients.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect} from 'react'
+import React from 'react'
 import Mathew from '../assets/images/mathew.png'
 import ellipse from '../assets/images/Ellipse 94.png'
 import dot from '../assets/images/Dot.png'
 import clients from '../assets/images/clients.png'
 import ellipse1 from '../assets/images/Ellipse 86.png'
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
  
   
 const Clients = () => {
 
- useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
+
   return (
     <section className='dark:bg-black'>
 <div className="container ">
@@ -22,18 +19,18 @@ const Clients = () => {
    <p className='text-[rgb(117,117,117)] font-roboto text-[18px] text-center mt-[183px] '>Several selected clients, who already believe in our service.</p>
 <div className='flex gap-[67px]'>
   <div className='relative dark:bg-red-100/10 rounded-full p-3 transition'>
-  <img src={Mathew} alt="" className='mt-[113px] dark:brightness-75'/>
-  <img src={ellipse} alt="" className='absolute left-[37px] top-[405px] z-[-1] dark:invert'/>
+  <img src={Mathew} alt="" className='mt-[113px] dark:brightness-75' data-aos="zoom-in-up"/>
+  <img src={ellipse} alt="" className='absolute left-[37px] top-[405px] z-[-1] dark:invert' />
   <img src={dot} alt="" className='absolute top-[102px] left-[313px] z-[-1] dark:invert'/>
 
 </div>
 <div className='mt-[197px]'>
   <p className='font-roboto font-semibold text-[24px] text-[#000000] mb-[23px] dark:text-white'>Matthew Paul</p>
-  <p className='w-[412px] h-[78px] text-[#565656]'>Perfect, very good job! Thank you for the amazing design and work. Really impressed with the high quality and quick turnaround time. Highly recommend.</p>
+  <p className='w-[412px] h-[78px] text-[#565656] dark:font-semibold'>Perfect, very good job! Thank you for the amazing design and work. Really impressed with the high quality and quick turnaround time. Highly recommend.</p>
 
 </div>
 <div className='mt-[113px] relative'>
-  <img src={clients} alt="" />
+  <img src={clients} alt="" data-aos="zoom-in-down"/>
   <img src={ellipse1} alt="" className='w-[184px] h-[184px] absolute top-[100px] left-[35px] z-[-1]'  />
 </div>
 </div>
